3e579c2eb6ee1a111ddf7a0b700681b54758184f
Xehny <105782952+Xehny@users.noreply.github.com>
2022-10-25 09:23:21 +1300

    Testing loading xml

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-echo("This is working");
+if (file_exists("../xml/catalogue.xml")) {
+    $catalogue = simplexml_load_file("../xml/catalogue.xml");
+}
+else {
+    exit("Failed to open catalogue.xml");
+}
+
+echo $catalogue->game[0];
 ?>
