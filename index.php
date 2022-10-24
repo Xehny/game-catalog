8c898d301fc542df4c7523cd7641c7bd9b9dfccf
Xehny <105782952+Xehny@users.noreply.github.com>
2022-10-25 10:24:37 +1300

    Updated catalogue

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
 <?php
-if (file_exists("../xml/catalogue.xml")) {
-    $catalogue = simplexml_load_file("../xml/catalogue.xml");
+if (file_exists("xml/catalogue.xml")) {
+    $catalogue = simplexml_load_file("xml/catalogue.xml");
 }
 else {
     exit("Failed to open catalogue.xml");
 }
 
-echo $catalogue->game[0];
+echo $catalogue->game[0]->title;
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <title>Assignment 4</title>
+    </head>
+    <body>
+    </body>
+</html>
