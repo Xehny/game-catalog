1dd55a4c8110089842f2541ce8bcd5f669c622b2
Xehny <105782952+Xehny@users.noreply.github.com>
2022-10-26 22:13:10 +1300

    Search works

--- js/script.js
+++ js/script.js
@@ -10,6 +10,9 @@ if (fileName == "index.php" || fileName == "") {
     let rating = document.getElementById("rating");
     rating.addEventListener("click", function() { sort(2); })
 
+    const search = document.getElementById("SearchBar");
+    search.addEventListener("keyup", function() { searching(); })
+
     const games = document.getElementsByClassName("game");
 
     const buttonPressed = e => {
@@ -26,44 +29,22 @@ if (fileName == "index.php" || fileName == "") {
         form.submit();
     }
 
-    let gameTitles = [];
-
     for (let game of games) {
         game.addEventListener("click", buttonPressed);
-        gameTitles.push(game.firstChild.innerHTML);
     }
-
-    const searchInput = document.querySelector("[data-search]")
-
-    searchInput.addEventListener("input", (e) => {
-        const val = e.target.value
-        gameTitles.forEach(title => {
-            const isVisible = title.includes(val);
-            getElementById(title).parentNode;
-        });
-        for(var y = 0; y < (gameTitles.length -1), y++;){
-
-            //games[y].style.display = 'none';
-
-            //games[y].firstChild.innerHTML
-            if(gameTitles[y].includes(value)){
-                games[y].style.display = 'block'
-            }
-            else{
-                console
-                games[y].style.display = 'hide'
-            }
-        }
-        
-    })
 }
+
+let sortBoolean = new Array(4).fill(false);
+sortBoolean[3] = true;
+sortBoolean[2] = true;
 
 function sort(col) {
     if(col > 2){
         alert("wtf")
         return;
     }
-    var table, i, rowA, rowB, unfinished;
+    col = col + 1;
+    var table, i, rowA, rowB, rowX, unfinished;
     table = document.getElementById("GameTable");
     unfinished = true;
 
@@ -80,7 +61,9 @@ function sort(col) {
             rowA = rows[i].getElementsByTagName("TD")[col];
             rowB = rows[i + 1].getElementsByTagName("TD")[col];
 
-            if(col == 2){
+            //Reverse sort
+            if(sortBoolean[col]){
+                // Check if rows need to be swapped
                 if (rowA.innerHTML.toLowerCase() < rowB.innerHTML.toLowerCase())
                 {
                     // If yes, mark Switch as needed and break loop
@@ -88,8 +71,9 @@ function sort(col) {
                     break;
                 }
             }
-            else{
-                // Check if 2 rows need to be switched
+            //Forwards sort
+            else if (!sortBoolean[col]){
+                // Check if rows need to be swapped
                 if (rowA.innerHTML.toLowerCase() > rowB.innerHTML.toLowerCase())
                 {
                     // If yes, mark Switch as needed and break loop
@@ -104,14 +88,57 @@ function sort(col) {
             unfinished = true;
         }
     }
-    var rowX = rows[0].getElementsByTagName("TD")[col]
-    if(col == 0){
-        rowX.innerHTML= "▼ Title";
-    }
-    else if(col == 1){
-        rowX.innerHTML= "▼ Price";
+    let tempBool = !sortBoolean[col];
+    sortBoolean = new Array(4).fill(false);
+    sortBoolean[3] = true;
+    sortBoolean[2] = true;
+    sortBoolean[col] = tempBool;
+
+    var rowX = rows[0].getElementsByTagName("TH")
+    if(col == 1){
+        rowX[2].innerHTML= "Price";
+        rowX[3].innerHTML= "Rating";
+
+        if(!sortBoolean[1])
+            rowX[1].innerHTML= "Title ▲";
+        else
+            rowX[1].innerHTML= "Title ▼";
     }
     else if(col == 2){
-        rowX.innerHTML= "▼ Rating";
+        rowX[1].innerHTML= "Title";
+        rowX[3].innerHTML= "Rating";
+
+        if(sortBoolean[2])
+            rowX[2].innerHTML= "Price ▲";
+        else
+            rowX[2].innerHTML= "Price ▼";
+    }
+    else if(col == 3){
+        rowX[1].innerHTML= "Title";
+        rowX[2].innerHTML= "Price";
+
+        if(sortBoolean[3])
+            rowX[3].innerHTML= "Rating ▲";
+        else
+            rowX[3].innerHTML= "Rating ▼";
+    }
+}
+
+function searching() {
+    var searchVal, table, tr, td, i, txtValue;
+    searchVal = document.getElementById("SearchBar").value;
+    table = document.getElementById("GameTable");
+    tr = table.getElementsByTagName("TR");
+
+    for (i = 1; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("TD")[1];
+        if (td) {
+            txtValue = td.innerHTML;
+            if (txtValue.toUpperCase().indexOf(searchVal.toUpperCase()) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }       
     }
 }
